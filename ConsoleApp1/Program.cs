e32b7813826af98dbcbe5cddffdb6fcca3a876b5
danie <danie@Y4IRDANIEL22>
2023-10-11 08:41:54 -0600

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class program
+{
+    const int maxf = 3;
+    const int maxc = 5;
+
+    static void Main(string[] args)
+    {
+        float[,] a = new float[maxf, maxc];
+        int f, c;
+
+        for (f = 0; f < maxf; f++)
+        {
+            for (c = 0; c < maxc; c++)
+            {
+                a[f, c] = float.Parse(Console.ReadLine());
+            }
+        }
+
+        for (f = 0; f < maxf; f++)
+        {
+            for (c = 0; c < maxc; c++)
+            {
+                Console.Write(a[f, c] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
